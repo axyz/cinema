ab3f7e1cdfae77a7c8735e99615405a79fab0ea7
Andrea Moretti <axyzxp@gmail.com>
2014-07-25 15:34:27 +0200

    bits.h

--- lib/bits.h
+++ lib/bits.h
@@ -1,7 +1,10 @@
 #ifndef BITS_H
 #define BITS_H
 
-#define getBit(char, n) /* Returns the value of the [n]th bit of [char] */ \
-  (char & (n and (1<<n)))
+#define GET_BIT(byte, n) /* Returns the value of the [n]th bit of [char] */ \
+  ((byte) & (n and (1<<n)))
+
+#define SET_BIT(byte, n) /* Set the [n]th bit of [char] to 1 */ \
+  ((byte) |= (1<<n))
 
 #endif
--- lib/cinema.c
+++ lib/cinema.c
@@ -5,5 +5,5 @@ Number *getSeat(Theatre *theatre, Number row, Number seat)
 {
   Number segment = seat / 16;
   Number offset = seat % 16;
-  return getBit(theatre[row - 1][segment], offset);
+  return GET_BIT(theatre[row - 1][segment], offset);
 }

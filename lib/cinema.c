a97510d0888f8add9a1b5563373ea1d47df08c0a
Andrea Moretti <axyzxp@gmail.com>
2014-07-25 14:21:53 +0200

    getSeat

--- lib/cinema.c
+++ lib/cinema.c
@@ -1,3 +1,8 @@
 #include "cinema.h"
 
-
+char *getSeat(*theatre, row, seat)
+{
+  int segment = seat / 16;
+  int offset = seat % 16;
+  return getBit(theatre[row - 1][segment], offset);
+}
--- lib/cinema.h
+++ lib/cinema.h
@@ -2,6 +2,10 @@
 #define CINEMA_H
 
 #define getBit(char, n) /* Returns the value of the [n]th bit of [char] */ \
-    (char & (n and (1<<n)))
+  (char & (n and (1<<n)))
+
+typedef char[16][16] theatre; 
+
+char getSeat(*theatre, row, seat);
 
 #endif

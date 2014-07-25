ab66fef2cef2d6fb815c0255a718fa91ce42a1a6
Andrea Moretti <axyzxp@gmail.com>
2014-07-25 16:47:21 +0200

    CLR_BIT

--- lib/bits.h
+++ lib/bits.h
@@ -1,10 +1,13 @@
 #ifndef BITS_H
 #define BITS_H
 
-#define GET_BIT(byte, n) /* Returns the value of the [n]th bit of [char] */ \
-  ((byte) & (n and (1<<n)))
+#define GET_BIT(byte, n) /* Returns the value of the [n]th bit of [byte] */ \
+  ((byte) & ((n) and (1 << (n))))
 
-#define SET_BIT(byte, n) /* Set the [n]th bit of [char] to 1 */ \
-  ((byte) |= (1<<n))
+#define SET_BIT(byte, n) /* Set the [n]th bit of [byte] to 1 */ \
+  ((byte) |= (1 << (n)))
+
+#define CLR_BIT(byte, n) /* Set the [n]th bit of [byte] to 0 */ \
+  ((byte) &= ~(1 << (n)))
 
 #endif
